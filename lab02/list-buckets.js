15590cd7ee53f7da2be6a9d4b465dba73846ab6f
jipx <pengxiang00@gmail.com>
2023-03-10 09:58:09 +0800

    Update list-buckets.js

--- lab02/list-buckets.js
+++ lab02/list-buckets.js
@@ -1,7 +1,9 @@
 const AWS = require('aws-sdk');
-const session = new AWS.Session();
+//creating an instance of the S3 class using new AWS.S3(). 
+//The apiVersion and region parameters are passed in during initialization, 
 const s3 = new AWS.S3({apiVersion: '2006-03-01', region: 'us-east-1'});
 
+ //listBuckets() function to retrieve a list of all S3 buckets in the account
 s3.listBuckets(function(err, data) {
   if (err) {
     console.log("Error", err);
